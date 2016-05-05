67091996462fdb3a681b41aba66a3f68015f2036
Fouad Bakour <fouad.b@euriskomobility.com>
2016-05-05 18:10:05 +0300

    Added xcode

---
+++ testapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testapp
+//
+//  Created by Shyreen on 4/21/16.
+//  Copyright © 2016 Eurisko Mobility. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testapp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testapp
+//
+//  Created by Shyreen on 4/21/16.
+//  Copyright © 2016 Eurisko Mobility. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
